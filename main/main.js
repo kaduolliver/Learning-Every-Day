cdbdd2eee557b6e8d9791c1e99c3c821cdfc5e7c
Carlos Eduardo <srdindong@gmail.com>
2024-10-24 18:57:19 -0300

    third-card-add

---
+++ main/main.js
@@ -0,0 +1,12 @@
+const topBar = document.querySelector('.top-bar');
+
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    // Define uma distância do topo para exibir a barra (ajuste conforme necessário)
+    if (scrollTop < 100) {
+        topBar.style.top = '0'; // Exibe a barra
+    } else {
+        topBar.style.top = '-100px'; // Esconde a barra
+    }
+});
