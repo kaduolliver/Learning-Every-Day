a4357027397be1435a9a5e9508ebfcef03635bb0
Carlos Eduardo <srdindong@gmail.com>
2024-10-25 22:16:58 -0300

    bottom-bar-audio-add

--- main/main.js
+++ main/main.js
@@ -3,10 +3,15 @@ const topBar = document.querySelector('.top-bar');
 window.addEventListener('scroll', function() {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-    // Define uma dist‚ncia do topo para exibir a barra (ajuste conforme necess·rio)
+    // Define uma dist√¢ncia do topo para exibir a barra (ajuste conforme necess√°rio)
     if (scrollTop < 100) {
         topBar.style.top = '0'; // Exibe a barra
     } else {
         topBar.style.top = '-100px'; // Esconde a barra
     }
 });
+
+const titulo = document.querySelector("h1");
+titulo.textContent = "Learning Every Day";
+
+
